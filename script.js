aad63f60f6aea28d68c3595ba6faab8dee3781ee
lasernoy <cloudy.nny07@gmail.com>
2024-10-03 21:26:52 +0800

    d3-fetch, promise

--- script.js
+++ script.js
@@ -33,36 +33,19 @@ function createChoroplethMap(data1, data2) {
 document.addEventListener("DOMContentLoaded", () => {
 	console.log("f e t c h i n g\t\t d a t a");
 
-	// XHR objects for requesting the us education and county datasets, respectively.
-	const request1 = new XMLHttpRequest();
-	const request2 = new XMLHttpRequest();
-
 	// URLs for US education and county data.
 	const url1 = "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json";
 	const url2 = "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json";
 
-	// requests
-	request1.open("GET", url1, true);
-	request1.send();
-	request2.open("GET", url2, true);
-	request2.send();
-
-	// storage for the datasets
-	let json1 = null;
-	let json2 = null;
-
-	// Request 1 returned
-	request1.onload = () => {
-		console.log("request 1 successful");
-		json1 = JSON.parse(request1.responseText);
-		console.log("US Education:", json1);
-		if (json1 && json2) createChoroplethMap(json1, json2);
-	};
-	// Request 2 returned
-	request2.onload = () => {
-		console.log("request 2 successful");
-		json2 = JSON.parse(request2.responseText);
-		console.log("US County:", json2);
-		if (json1 && json2) createChoroplethMap(json1, json2);
-	};
+	// Fetches all dataset
+	Promise
+		.all([
+			d3.json(url1), 
+			d3.json(url2)
+		])
+		.then(data => {
+			console.log("Dataset fetched:\n", data);
+			createChoroplethMap(data[0], data[1]);
+		})
+		.catch(err => console.log("Error! lacking dataset:", err));
 });
