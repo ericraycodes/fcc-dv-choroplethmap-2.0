c4e248a61273dbaf2b67a87322a3e4778f8e7986
lasernoy <cloudy.nny07@gmail.com>
2024-11-15 15:02:10 +0800

    geoIdentity for preprojected geometry

--- script.js
+++ script.js
@@ -4,9 +4,13 @@
 
 /** DRAW CHOROPLETH
  	* 1. Append the page's title and description texts.
- 	* 2.
+ 	* 2. Setup the SVG's dimensions.
+ 	* 3. Prepare the map's geodata.
+ 	* 4. Select the map projection.
+ 	* 5. Create the path generator.
+ 	* 6. Draw the map in to the SVG.
  	*/
-function createChoroplethMap(data1, data2) {
+function createChoroplethMap(USEd, USMap) {
 	console.log("C H O R O P L E T H");
 
 
@@ -17,10 +21,51 @@ function createChoroplethMap(data1, data2) {
 	`);
 
 
-	// SVG
+	// SVG DIMENSIONS
+	const height = 600;
+	const width = 1000;
 	const svg = d3.select("#choropleth-box")
 		.append("svg")
-		.attr("id", "choropleth");
+		.attr("id", "choropleth")
+		.attr("height", height)
+		.attr("width", width);
+
+
+
+
+	// GEODATA: convert TopoJSON data to GeoJSON data
+	const nation = topojson.feature(USMap, USMap.objects.nation);
+	const states = topojson.feature(USMap, USMap.objects["states"]);
+	const counties = topojson.feature(USMap, USMap.objects["counties"]);
+	console.log("geojson nation:", nation);
+	console.log("geojson states:", states);
+	console.log("geojson counties:", counties);
+	const usGeoJSON = {
+		"type"		: "FeatureCollection",
+		"features"	: nation.features.concat(states.features).concat(counties.features)
+	};
+	
+	// PROJECTION
+	const pad = 5;
+	const projection = d3.geoIdentity()
+		.fitExtent([[pad, pad], [width - pad, height - pad]], usGeoJSON);
+
+
+	// PATH GENERATOR
+	const path = d3.geoPath().projection(projection);
+	console.log("path generator:", path(nation), path(states), path(counties));
+
+
+
+	// DRAW
+	svg.append("g")
+		.selectAll("path")
+		.data(usGeoJSON.features)
+		.enter()
+		.append("path")
+			.attr("d", path)
+			.attr("fill", "none")
+			.attr("stroke", "blue");
 }
 
 
@@ -44,7 +89,7 @@ document.addEventListener("DOMContentLoaded", () => {
 			d3.json(url2)
 		])
 		.then(data => {
-			console.log("Dataset fetched:\n", data);
+			console.log("Dataset fetched:", data[0], data[1]);
 			createChoroplethMap(data[0], data[1]);
 		})
 		.catch(err => console.log("Error! lacking dataset:", err));
